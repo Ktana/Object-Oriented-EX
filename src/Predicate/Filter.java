d2e44b8b9459f3fcd3e6789b7f09f21be8ce5801
AlexandraFishman <33634387+AlexandraFishman@users.noreply.github.com>
2018-01-02 21:19:55 +0200

    Add files via upload

--- src/Predicate/Filter.java
+++ src/Predicate/Filter.java
@@ -4,20 +4,20 @@ import java.io.Serializable;
 
 @SuppressWarnings("serial")
 public class Filter implements Serializable{
-	boolean TimechkBx;
-	String TIME_MinVal;
-	String TIME_MaxVal;
-	boolean AltchkBx;
-	String ALT_MinVal;
-	String ALT_MaxVal;
-	boolean LATchkBx;
-	String LAT_MinVal;
-	String LAT_MaxVal;
-	boolean LONchkBx;
-	String LON_MinVal;
-	String LON_MaxVal;
-	boolean IDchkBx;
-	String ID_MinVal;
+	public boolean TimechkBx;
+	public String TIME_MinVal;
+	public String TIME_MaxVal;
+	public boolean AltchkBx;
+	public String ALT_MinVal;
+	public String ALT_MaxVal;
+	public boolean LATchkBx;
+	public String LAT_MinVal;
+	public String LAT_MaxVal;
+	public boolean LONchkBx;
+	public String LON_MinVal;
+	public String LON_MaxVal;
+	public boolean IDchkBx;
+	public String ID_MinVal;
 	
 	public Filter(){
 		 this.TimechkBx = false;
@@ -59,4 +59,15 @@ public class Filter implements Serializable{
 		this.ID_MinVal = ID_MinVal;
 
 	}
+
+	@Override
+	public String toString() {
+		return "Filter [TimechkBx=" + TimechkBx + ", TIME_MinVal=" + TIME_MinVal + ", TIME_MaxVal=" + TIME_MaxVal
+				+ ", AltchkBx=" + AltchkBx + ", ALT_MinVal=" + ALT_MinVal + ", ALT_MaxVal=" + ALT_MaxVal + ", LATchkBx="
+				+ LATchkBx + ", LAT_MinVal=" + LAT_MinVal + ", LAT_MaxVal=" + LAT_MaxVal + ", LONchkBx=" + LONchkBx
+				+ ", LON_MinVal=" + LON_MinVal + ", LON_MaxVal=" + LON_MaxVal + ", IDchkBx=" + IDchkBx + ", ID_MinVal="
+				+ ID_MinVal + "]";
+	}
+	
+	
 }
