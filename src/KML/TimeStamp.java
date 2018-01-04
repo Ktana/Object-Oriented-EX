9eebf1f27a7b93e7f7812cc40d814b2fb7e72165
AlexandraFishman <33634387+AlexandraFishman@users.noreply.github.com>
2018-01-04 19:22:35 +0200

    Add files via upload

--- src/KML/TimeStamp.java
+++ src/KML/TimeStamp.java
@@ -5,6 +5,8 @@ import java.time.format.DateTimeFormatter;
 import org.jsefa.xml.annotation.XmlDataType;
 import org.jsefa.xml.annotation.XmlElement;
 
+import Global.DateFormat;
+
 @XmlDataType(defaultElementName = "TimeStamp")
 public class TimeStamp {
 	@XmlElement(pos = 1)
@@ -16,8 +18,10 @@ public class TimeStamp {
 
 	public TimeStamp(String date)
 	{
-		this.when = LocalDateTime.parse(date , DateTimeFormatter.ofPattern( "yyyy-MM-dd HH:mm:ss" )
-			).toString()+"Z";	
+//		this.when = LocalDateTime.parse(date , DateTimeFormatter.ofPattern( "yyyy-MM-dd HH:mm:ss" )
+//			).toString()+"Z";
+		this.when = DateFormat.adjustTime(date
+				).toString()+"Z";
 	}
 
 
