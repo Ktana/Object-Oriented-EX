ae4056e25e5a658de1b0e834e3524dae505131ce
AlexandraFishman <33634387+AlexandraFishman@users.noreply.github.com>
2018-01-05 11:44:19 +0200

    Add files via upload

--- src/KML/Placemark.java
+++ src/KML/Placemark.java
@@ -4,7 +4,11 @@ import org.jsefa.xml.annotation.XmlDataType;
 import org.jsefa.xml.annotation.XmlElement;
 
 @XmlDataType(defaultElementName = "Placemark")
-
+/**
+ * A class to get the automatic KML generator to create a place mark tag with all it's fields
+ * @author Alex Fishman
+ *
+ */
 public class Placemark {
 	@XmlElement(pos = 1)
 	String name;
@@ -27,8 +31,19 @@ public class Placemark {
 	public Point getPoint(){return this.Point;}
 	public TimeStamp getDate(){return this.TimeStamp;}
 	
+	/**
+	 * Blank constructor to use the interface of Jsefa
+	 */
 	public Placemark(){}
 	
+	/**
+	 * Constructor from data 
+	 * @param name
+	 * @param description
+	 * @param styleUrl
+	 * @param point
+	 * @param timeStamp
+	 */
 	public Placemark(String name,String description,String styleUrl,Point point,TimeStamp timeStamp){
 		this.name = name;
 		this.description = description;
