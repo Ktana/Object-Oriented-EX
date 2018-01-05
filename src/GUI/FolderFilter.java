1c15a45219ed8a8c943cd63269e674dd549e4de3
AlexandraFishman <33634387+AlexandraFishman@users.noreply.github.com>
2018-01-05 11:27:48 +0200

    Add files via upload

--- src/GUI/FolderFilter.java
+++ src/GUI/FolderFilter.java
@@ -1,7 +1,11 @@
 package GUI;
 
 import java.io.File;
-
+/**
+ * Was supposed to filter the folder from the non-CSV's - CURRENTLY NOT USED, found a better solution!
+ * @author Alex Fishman
+ *
+ */
 public class FolderFilter extends javax.swing.filechooser.FileFilter {
       @Override
       public boolean accept( File file ) {
